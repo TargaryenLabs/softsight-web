22eed3e2f308b898406a4e3fe011832b8610914c
SazaO_o <121901716+Saza-dev@users.noreply.github.com>
2025-04-13 01:32:07 +0530

    HeroSection

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -3,18 +3,22 @@ import Image from "next/image";
 const Navbar = () => {
   return (
     <div className="w-full items-center justify-center flex">
-      <div className="mt-[20px] bg-blue-100 flex w-[1400px] h-[80px] items-center rounded-3xl justify-between pl-[50px] pr-[80px] shadow-2xl">
+      <div className="mt-[20px] bg-blue-50 flex w-[1400px] h-[80px] items-center rounded-3xl justify-between pl-[50px] pr-[80px] shadow-2xl">
         <div className="flex text-[32px] gap-3 font-[640] items-center">
+          {/* Logo Start */}
           <a href="/" className="flex items-center gap-4">
             <div>
-              <Image alt="Logo" src="/logo.png" width={60} height={60} />
+              <Image alt="Logo" src="/images/logo.png" width={60} height={60} />
             </div>
             <div className="flex gap-1">
               <p>Soft</p>
               <p className="">Sight</p>
             </div>
           </a>
+          {/* Logo End */}
         </div>
+
+        {/* Links Start*/}
         <div className="flex gap-[160px] text-[18px] font-[400]">
           <a
             href="/"
@@ -35,6 +39,7 @@ const Navbar = () => {
             About us
           </a>
         </div>
+        {/* Links End*/}
       </div>
     </div>
   );
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,8 +1,21 @@
+import HeroSection from "../components/HomePage/HeroSection";
 import Navbar from "../components/NavBar";
 
 const HomePage = () => {
     return (
-        <Navbar/>
+        <div>
+
+            <div>
+            <Navbar/>
+            </div>
+
+            <div>
+                <HeroSection/>
+            </div>
+
+        </div>
+        
+
     );
 }
  
