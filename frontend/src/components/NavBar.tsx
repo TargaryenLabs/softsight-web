919301761be5a732a4866ba6a5ea4e1662661464
SazaO_o <121901716+Saza-dev@users.noreply.github.com>
2025-04-13 00:50:06 +0530

    NavBar

---
+++ frontend/src/components/NavBar.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+
+const Navbar = () => {
+  return (
+    <div className="w-full items-center justify-center flex">
+      <div className="mt-[20px] bg-blue-100 flex w-[1400px] h-[80px] items-center rounded-3xl justify-between pl-[50px] pr-[80px] shadow-2xl">
+        <div className="flex text-[32px] gap-3 font-[640] items-center">
+          <a href="/" className="flex items-center gap-4">
+            <div>
+              <Image alt="Logo" src="/logo.png" width={60} height={60} />
+            </div>
+            <div className="flex gap-1">
+              <p>Soft</p>
+              <p className="">Sight</p>
+            </div>
+          </a>
+        </div>
+        <div className="flex gap-[160px] text-[18px] font-[400]">
+          <a
+            href="/"
+            className="hover:text-[#1E73E8] transition-all duration-500"
+          >
+            Home
+          </a>
+          <a
+            href="/forecast"
+            className="hover:text-[#1E73E8] transition-all duration-500"
+          >
+            Forecast
+          </a>
+          <a
+            href="/about-us"
+            className="hover:text-[#1E73E8] transition-all duration-500"
+          >
+            About us
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+import Navbar from "../components/NavBar";
+
+const HomePage = () => {
+    return (
+        <Navbar/>
+    );
+}
+ 
+export default HomePage;
